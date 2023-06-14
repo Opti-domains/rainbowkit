cca0dfabf070d37c7b4397115576565a6f71baf9
Chomtana <Chomtana001@gmail.com>
2023-06-14 09:35:15 +0700

    Draft universal registry

--- packages/rainbowkit/src/hooks/useMainnetEnsName.ts
+++ packages/rainbowkit/src/hooks/useMainnetEnsName.ts
@@ -1,14 +1,27 @@
-import { useEnsName } from 'wagmi';
+import { useEnsName, useNetwork } from 'wagmi';
 import { useMainnet } from './useMainnet';
+import { useUniversalReverseResolver } from './useUniversalEnsRegistry.ts';
 
 export function useMainnetEnsName(address: string | undefined) {
-  const { chainId, enabled } = useMainnet();
+  const { chainId } = useNetwork();
+  const { chainId: mainnetChainId, enabled } = useMainnet();
 
+  const { data: universalResolver, isLoading, isError } = useUniversalReverseResolver({ address, chainId })
+  
   const { data: ensName } = useEnsName({
-    address,
+    address: isLoading || isError || !universalResolver || universalResolver === '0x0000000000000000000000000000000000000000' ? undefined : address,
     chainId,
+    enabled,
+    universalResolver
+  });
+
+  const { data: ensNameMainnet } = useEnsName({
+    address,
+    chainId: mainnetChainId,
     enabled,
   });
 
+  if (isLoading) return undefined;
+  if (isError) return ensNameMainnet;
   return ensName;
 }
