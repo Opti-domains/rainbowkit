9c1d530457bce4f2124159145bb2087b3f2b4f76
Chomtana <Chomtana001@gmail.com>
2023-06-17 01:31:18 +0700

    feat: support optidomains universalEnsRegistry

--- packages/rainbowkit/src/hooks/useMainnetEnsAvatar.ts
+++ packages/rainbowkit/src/hooks/useMainnetEnsAvatar.ts
@@ -6,14 +6,26 @@ export function useMainnetEnsAvatar(name: string | null | undefined) {
   const { chain } = useNetwork();
   const { chainId: mainnetChainId, enabled } = useMainnet();
 
-  const { data: getRegistryByNameResult, isLoading, isError } = useUniversalEnsRegistryResolver({ name, chainId: chain.id })
+  const {
+    data: getRegistryByNameResult,
+    isError,
+    isLoading,
+  } = useUniversalEnsRegistryResolver({ chainId: chain.id, name });
 
-  const universalResolver = getRegistryByNameResult ? getRegistryByNameResult[1] : undefined;
+  const universalResolver = getRegistryByNameResult
+    ? getRegistryByNameResult[1]
+    : undefined;
 
   const { data: ensAvatar } = useEnsAvatar({
     chainId: chain.id,
     enabled,
-    name: isLoading || isError || !universalResolver || universalResolver === '0x0000000000000000000000000000000000000000' ? undefined : name,
+    name:
+      isLoading ||
+      isError ||
+      !universalResolver ||
+      universalResolver === '0x0000000000000000000000000000000000000000'
+        ? undefined
+        : name,
     universalResolverAddress: universalResolver,
   });
 
@@ -24,6 +36,11 @@ export function useMainnetEnsAvatar(name: string | null | undefined) {
   });
 
   if (isLoading) return undefined;
-  if (isError || !universalResolver || universalResolver === '0x0000000000000000000000000000000000000000') return ensAvatarMainnet;
+  if (
+    isError ||
+    !universalResolver ||
+    universalResolver === '0x0000000000000000000000000000000000000000'
+  )
+    return ensAvatarMainnet;
   return ensAvatar;
 }
--- packages/rainbowkit/src/hooks/useMainnetEnsName.ts
+++ packages/rainbowkit/src/hooks/useMainnetEnsName.ts
@@ -6,10 +6,20 @@ export function useMainnetEnsName(address: string | undefined) {
   const { chain } = useNetwork();
   const { chainId: mainnetChainId, enabled } = useMainnet();
 
-  const { data: universalResolver, isLoading, isError } = useUniversalEnsReverseResolver({ address, chainId: chain.id })
-  
+  const {
+    data: universalResolver,
+    isError,
+    isLoading,
+  } = useUniversalEnsReverseResolver({ address, chainId: chain.id });
+
   const { data: ensName } = useEnsName({
-    address: isLoading || isError || !universalResolver || universalResolver === '0x0000000000000000000000000000000000000000' ? undefined : address,
+    address:
+      isLoading ||
+      isError ||
+      !universalResolver ||
+      universalResolver === '0x0000000000000000000000000000000000000000'
+        ? undefined
+        : address,
     chainId: chain.id,
     enabled,
     universalResolverAddress: universalResolver,
@@ -22,6 +32,11 @@ export function useMainnetEnsName(address: string | undefined) {
   });
 
   if (isLoading) return undefined;
-  if (isError || !universalResolver || universalResolver === '0x0000000000000000000000000000000000000000') return ensNameMainnet;
+  if (
+    isError ||
+    !universalResolver ||
+    universalResolver === '0x0000000000000000000000000000000000000000'
+  )
+    return ensNameMainnet;
   return ensName;
 }
